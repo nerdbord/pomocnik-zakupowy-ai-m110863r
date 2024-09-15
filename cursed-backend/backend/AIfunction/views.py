1d028311fb950a30eb2c8252511b4d870d421d7b
Krystian <matematyk45@o2.pl>
2024-09-15 18:10:51 +0200

    adding some error handling

--- cursed-backend/backend/AIfunction/views.py
+++ cursed-backend/backend/AIfunction/views.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 import concurrent.futures
 import re
 import os
+from requests.exceptions import HTTPError
 
 
 app = FirecrawlApp(os.getenv("Fckey"))
@@ -24,7 +25,10 @@ def responding(markdown, url):
 
 
 def scrape_and_process_url(url):
-    scrape_status = app.scrape_url(url, params={'formats': ["markdown"]})
+    try:
+        scrape_status = app.scrape_url(url, params={'formats': ["markdown"]})
+    except HTTPError:
+        "Something went wrong with getting the offers, please try in a minute. If problem persists, contact RAPID ARCHITECTS :)"
     markdown = scrape_status['markdown'][:24000]
     return responding(markdown, url).choices[0].message.content
 
